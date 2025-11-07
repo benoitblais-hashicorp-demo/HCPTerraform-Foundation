c99d980de92b22e6bc51dfd737d5a8cc4528f66b
Benoit Blais <benoit.blais@hashicorp.com>
2025-11-07 15:40:33 -0500

    version

--- versions.tf
+++ versions.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "hashicorp/tfe"
       version = "~>0.70"
     }
-    github = {
-      source  = "integrations/github"
-      version = "~>6.6.0"
-    }
   }
 
   required_version = ">= 1.13.0"
